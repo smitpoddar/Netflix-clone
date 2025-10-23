612fd980c4ea817ff0e91d17e496ed891e4975d7
Smit kumar <141864912+smitpoddar@users.noreply.github.com>
2025-10-23 20:35:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App(){
     <ToastContainer theme="dark"/>
       <Routes>
         <Route path="/home" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/player/:id" element={<Player />} />
       </Routes>
     </div>
