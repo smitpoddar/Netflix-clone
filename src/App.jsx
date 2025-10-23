3def64e3b6eae3b8b1e198361fe866c25a85a0d5
Smit kumar <141864912+smitpoddar@users.noreply.github.com>
2025-10-23 20:27:22 +0530

    Change home route path from '/' to '/home'

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App(){
     <div>
     <ToastContainer theme="dark"/>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/player/:id" element={<Player />} />
       </Routes>
